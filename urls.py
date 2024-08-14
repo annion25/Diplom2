648c945c735fcf2d744063bb3cb6281f9b1d2ce2
Anna Evseenkova <annion25@gmail.com>
2024-08-14 17:54:11 -0400

    first commit

---
+++ urls.py
@@ -0,0 +1,10 @@
+
+
+BASE_URL = 'https://stellarburgers.nomoreparties.site'
+USER_CREATION_URL = '/api/auth/register'
+USER_LOGIN_URL = '/api/auth/login'
+USER_DELETE_UPDATE_URL = '/api/auth/user'
+ORDER_API = 'api/orders'
+INGREDIENTS_API = 'api/inredients'
+
+
